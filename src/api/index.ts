76b1d1856d7c9fc656fc3f18eeaf6f3aeeb5484f
NightDreams <rost090909@gmail.com>
2023-08-21 22:28:15 +0300

    delete headers

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,6 @@ import axios, { AxiosInstance } from "axios"
 
 const instance: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API,
-  headers: {
-    "Permissions-Policy": "interest-cohort=()",
-  },
 })
 
 export const userAPI = {
